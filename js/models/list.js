3ab6a6b69c69a81f4f3fb8247c78902fa848b3e9
asambvani <alexsambvani@gmail.com>
2017-07-22 00:09:02 -0400

    Added all functionality

--- js/models/list.js
+++ js/models/list.js
@@ -1,2 +1,23 @@
 // List Model
 
+let List = newList();
+
+function newList(){
+  let listCounter = 0;
+  let all = []
+
+  return class List{
+    constructor(title){
+      this.tasks = [];
+      this.title = title
+      this.id = listCounter++;
+      store.lists.push(this)
+    }
+    static find(id){
+      let returnList = store.lists.filter(function(list){
+        return list.id==id
+      })
+      return returnList
+    }
+  }
+}
--- js/models/task.js
+++ js/models/task.js
@@ -1,2 +1,19 @@
 // Task Model
+let store = {lists: [], tasks: []}
 
+let Task = newTask();
+
+function newTask(){
+  let taskCounter = 0;
+
+  return class Task{
+
+    constructor(description, priority,list){
+      this.id = taskCounter++;
+      this.description = description
+      this.priority = priority
+      this.list = list
+      store.tasks.push(this)
+    }
+  }
+}
